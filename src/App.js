a13feee10e35d7e770aaba21ab1c61fc3b1a1c9a
mazinger086 <mazinger_086@yahoo.com>
2020-10-22 18:59:46 -0300

    clase 64 mensaje de forma condicional

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, {Fragment, useState} from 'react';
+import Formulario from './components/Formulario';
+import Cita from './components/Cita';
+
+function App() {
+  
+  //Areglo de citas
+  const [citas, guardarCitas] = useState([]);
+
+  // Funcion que tome las citas actuales y agregue la nueva
+  const crearCita = cita => {
+     guardarCitas([...citas, cita])
+  }
+
+  //Funcion que elimina un cita por su id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita=> cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
+  //Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas'
+
+
+  return (
+    <Fragment>
+      <h1>Administrador de pacientes</h1>
+      <div className="container">
+     
+      <div className="row">
+        <div className="one-half column">
+        <Formulario 
+          crearCita={crearCita}
+
+        />
+        </div>
+        <div className="one-half column">
+        <h2>{titulo}</h2>
+          {
+            citas.map(cita => ( 
+              <Cita
+                  cita={cita}
+                  key={cita.id}
+                  eliminarCita={ () => eliminarCita(cita.id)}
+              />
+             ))
+          }
+        </div>
+      </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
